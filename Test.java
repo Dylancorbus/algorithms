8b31d952f9214c3ec8cafb507629897103e1076e
Dylancorbus <dylancorbus@outlook.com>
2020-06-09 20:28:04 -0700

    test

--- Test.java
+++ Test.java
@@ -35,6 +35,8 @@ public class Test {
             current = queue.poll();
 //            get the child node
 //            O(E)
+            String s = new String();
+
             for (int i = 0; i < current.childNodes.size(); i++) {
 //                add them to a map as we go
                 found.put(current.childNodes.get(i).name, current);
@@ -296,59 +298,130 @@ public class Test {
         return answer;
     }
 
-    public List<String[]> avail(List<List<String>> cal1, List<List<String>> cal2, List<String> rang1, List<String> rang2, int duration) {
-        //O(1)
-        List<String[]> unavailTimes = new ArrayList<>();
-        List<String[]> potentialMeetingTimes = new ArrayList<>();
+
+//    public List<String[]> avail(List<List<String>> cal1, List<List<String>> cal2, List<String> rang1, List<String> rang2, int duration) {
+//        //O(1)
+//        List<String[]> unavailTimes = new ArrayList<>();
+//        List<String[]> potentialMeetingTimes = new ArrayList<>();
+//        String previousStart = "";
+//        String previousEnd = "";
+//        int i = 0;
+//        int j = 0;
+//        int count = 0;
+//        //O(n + m)
+//        while(i < cal1.size() && j < cal2.size()) {
+//            //O(1)
+//            String[] unavailSlot = new String[2];
+//            String stime1 = cal1.get(i).get(0);
+//            String stime2 = cal2.get(j).get(0);
+//            String etime1 = cal1.get(i).get(1);
+//            String etime2 = cal2.get(j).get(1);
+//            //O(1)
+//            if(compareTime(stime1, stime2) == 1) {
+//                unavailSlot[0] = stime2;
+//            } else {
+//                unavailSlot[0] = stime1;
+//            }
+//            if(compareTime(etime1, etime2) == 1) {
+//                if(compareTime(stime1, etime2) == 1) {
+//                    unavailSlot[1] = etime2;
+//                    ++j;
+//                } else {
+//                    unavailSlot[1] = etime1;
+//                    ++i;
+//                }
+//            } else {
+//                unavailSlot[1] = etime2;
+//                ++i;
+//            }
+//            if(!unavailSlot[0].equals(previousStart) && !unavailSlot[1].equals(previousEnd)) {
+//                unavailTimes.add(unavailSlot);
+//                previousStart  = unavailSlot[0];
+//                previousEnd  = unavailSlot[1];
+//            }
+//            ++count;
+//        }
+//        System.out.println(count);
+//        unavailTimes.forEach(strings -> System.out.print(Arrays.toString(strings)));
+//        System.out.println();
+//        //O(n)
+//        for (int j2 = 0; j2 < unavailTimes.size() - 1; j2++) {
+//            String currentEnd = unavailTimes.get(j2)[1];
+//            String nextStart = unavailTimes.get(j2 + 1)[0];
+//            String[] availSlot = new String[2];
+//            availSlot[0] = currentEnd;
+//            availSlot[1] = nextStart;
+//            if(withinRange(rang1, rang2, availSlot[0], availSlot[1], duration)) {
+//                potentialMeetingTimes.add(availSlot);
+//            }
+//        }
+//
+//
+//        return potentialMeetingTimes;
+//    }
+    static class StringMeeting {
+        public String start;
+        public String end;
+
+        public StringMeeting() {}
+
+        public StringMeeting(String start, String end) {
+            this.start = start;
+            this.end = end;
+        }
+    }
+
+    public static List<StringMeeting> calendarMatching(
+            List<StringMeeting> calendar1,
+            StringMeeting dailyBounds1,
+            List<StringMeeting> calendar2,
+            StringMeeting dailyBounds2,
+            int meetingDuration) {
+        // Write your code here.
+        List<StringMeeting> unavailTimes = new ArrayList<>();
+        List<StringMeeting> potentialMeetingTimes = new ArrayList<>();
         String previousStart = "";
         String previousEnd = "";
         int i = 0;
         int j = 0;
-        int count = 0;
         //O(n + m)
-        while(i < cal1.size() && j < cal2.size()) {
+        while (i < calendar1.size() && j < calendar2.size()) {
             //O(1)
-            String[] unavailSlot = new String[2];
-            String stime1 = cal1.get(i).get(0);
-            String stime2 = cal2.get(j).get(0);
-            String etime1 = cal1.get(i).get(1);
-            String etime2 = cal2.get(j).get(1);
+            StringMeeting unavailSlot = new StringMeeting();
+            String stime1 = calendar1.get(i).start;
+            String stime2 = calendar2.get(j).start;
+            String etime1 = calendar1.get(i).end;
+            String etime2 = calendar2.get(j).end;
             //O(1)
-            if(compareTime(stime1, stime2) == 1) {
-                unavailSlot[0] = stime2;
+            if (compareTime(stime1, stime2) == 1) {
+                unavailSlot.start = stime2;
             } else {
-                unavailSlot[0] = stime1;
+                unavailSlot.start = stime1;
             }
-            if(compareTime(etime1, etime2) == 1) {
-                if(compareTime(stime1, etime2) == 1) {
-                    unavailSlot[1] = etime2;
+            if (compareTime(etime1, etime2) == 1) {
+                if (compareTime(stime1, etime2) == 1) {
+                    unavailSlot.end = etime2;
                     ++j;
                 } else {
-                    unavailSlot[1] = etime1;
+                    unavailSlot.end = etime1;
                     ++i;
                 }
             } else {
-                unavailSlot[1] = etime2;
+                unavailSlot.end = etime2;
                 ++i;
             }
-            if(!unavailSlot[0].equals(previousStart) && !unavailSlot[1].equals(previousEnd)) {
+            if (!unavailSlot.start.equals(previousStart) && !unavailSlot.end.equals(previousEnd)) {
                 unavailTimes.add(unavailSlot);
-                previousStart  = unavailSlot[0];
-                previousEnd  = unavailSlot[1];
+                previousStart = unavailSlot.start;
+                previousEnd = unavailSlot.start;
             }
-            ++count;
         }
-        System.out.println(count);
-        unavailTimes.forEach(strings -> System.out.print(Arrays.toString(strings)));
-        System.out.println();
         //O(n)
         for (int j2 = 0; j2 < unavailTimes.size() - 1; j2++) {
-            String currentEnd = unavailTimes.get(j2)[1];
-            String nextStart = unavailTimes.get(j2 + 1)[0];
-            String[] availSlot = new String[2];
-            availSlot[0] = currentEnd;
-            availSlot[1] = nextStart;
-            if(withinRange(rang1, rang2, availSlot[0], availSlot[1], duration)) {
+            String currentEnd = unavailTimes.get(j2).end;
+            String nextStart = unavailTimes.get(j2 + 1).start;
+            StringMeeting availSlot = new StringMeeting(currentEnd, nextStart);
+            if (withinRange(dailyBounds1, dailyBounds1, availSlot.start, availSlot.end, meetingDuration)) {
                 potentialMeetingTimes.add(availSlot);
             }
         }
@@ -357,11 +430,11 @@ public class Test {
         return potentialMeetingTimes;
     }
 
-    public boolean withinRange(List<String> rang1, List<String> rang2, String stime, String etime, int duration) {
-        String rSTime1 = rang1.get(0);
-        String rSTime2 = rang2.get(0);
-        String rETime1 = rang1.get(1);
-        String rETime2 = rang2.get(1);
+    public static boolean withinRange(StringMeeting rang1, StringMeeting rang2, String stime, String etime, int duration) {
+        String rSTime1 = rang1.start;
+        String rSTime2 = rang2.start;
+        String rETime1 = rang1.end;
+        String rETime2 = rang2.end;
         String[] timeArr1 = stime.split(":");
         String[] timeArr2 = etime.split(":");
         int time1 = Integer.parseInt(timeArr1[0]) * 60 + Integer.parseInt(timeArr1[1]);
@@ -377,7 +450,7 @@ public class Test {
         return true;
     }
 
-    public int compareTime(String comp1, String comp2) {
+    public static int compareTime(String comp1, String comp2) {
         if(comp1 == null) return 1;
         if(comp2 == null) return -1;
         String[] timeArr1 = comp1.split(":");
@@ -386,6 +459,136 @@ public class Test {
         int time2 = Integer.parseInt(timeArr2[0]) * 60 + Integer.parseInt(timeArr2[1]);
         if(time1 >= time2) return 1;
         else return -1;
+    }
+
+    public static List<Integer> spiralTraverse(int[][] array) {
+        int r = 0;
+        int c = 0;
+        List<Integer> answer = new ArrayList<>();
+        Set<Integer> checked = new HashSet<>();
+        while(r < array.length && c < array[r].length) {
+            int[] newVals = navigate(r, c, array, answer, checked);
+            checked.add(array[r][c]);
+        }
+
+        return answer;
+    }
+
+    static int[] navigate(int row, int col, int[][] array, List<Integer> answer, Set<Integer> checked) {
+        int[] newVals = new int[2];
+        if(checked.contains(array[row][col])) {
+            if(row == 0) {
+                ++row;
+            } else {
+                --row;
+            }
+            if(col == 0) {
+                ++col;
+            } else {
+                --col;
+            }
+            newVals[0] = row;
+            newVals[1] = col;
+            return newVals;
+        }
+        answer.add(array[row][col]);
+        if(col == array[row].length - 1) {
+            if(row == array.length - 1) {
+                --col;
+                newVals[0] = row;
+                newVals[1] = col;
+                return newVals;
+            }
+            ++row;
+            newVals[0] = row;
+            newVals[1] = col;
+            return newVals;
+        }
+        if(col == 0) {
+            if(row != 0) {
+                row = row == 1 ? col++ == 0 ? row : row : --row;
+                newVals[0] = row;
+                newVals[1] = col;
+                return newVals;
+            }
+            ++col;
+
+            newVals[0] = row;
+            newVals[1] = col;
+            return newVals;
+        }
+        if(row == array.length - 1) {
+            --col;
+        } else {
+            ++col;
+        }
+        newVals[0] = row;
+        newVals[1] = col;
+        return newVals;
+    }
+
+    public static boolean isValidSubsequence(List<Integer> array, List<Integer> sequence) {
+        // Write your code here.
+
+            String arrS = array.toString().substring(1, array.toString().length());;
+            String arrS2 = sequence.toString().substring(1, sequence.toString().length());
+            if(arrS.contains(arrS2)) return true;
+        //space O(n)
+        Map<Integer, Integer> seqSet = new HashMap<>();
+        //O(n)
+
+        for(int i = 0; i < array.size(); i++) {
+            seqSet.put(array.get(i), i);
+        }
+        //O(m)
+        for(int i = 0; i < sequence.size(); i++) {
+            if(!seqSet.containsKey(sequence.get(i))) return false;
+            if(i < sequence.size() - 1) {
+                try {
+                    // if(sequence.get(i) == sequence.get(i + 1)) continue;
+                    if(seqSet.get(sequence.get(i)) > seqSet.get(sequence.get(i + 1))) return false;
+                    if(seqSet.get(sequence.get(i)) == seqSet.get(sequence.get(i + 1))) return false;
+                } catch(Exception e) {
+                    return false;
+                }
+            }
+        }
+
+        return true;
+    }
+
+    public static int[] smallestDifference(int[] arrayOne, int[] arrayTwo) {
+        // Write your code here.
+        //O(nlog(n))
+        Arrays.sort(arrayOne);
+        Arrays.sort(arrayTwo);
+        int i = 0;
+        int start = i;
+        int j = arrayTwo.length - 1;
+        int end = j;
+        int best = Integer.MAX_VALUE;
+        int[] smlDiff = new int[2];
+        while(i < arrayOne.length && j >= 0) {
+            int diff = arrayOne[i] - arrayTwo[j];
+            int newBest = Math.abs(diff);
+            if( newBest <= best) {
+                best = newBest;
+                smlDiff[0] = arrayOne[i];
+                smlDiff[1] = arrayTwo[j];
+            }
+            if(i == arrayOne.length - 1 || j == 0) {
+                i = ++start;
+                j = --end;
+                continue;
+            }
+            if(diff < 0) {
+                ++i;
+            }
+            if(diff > 0) {
+                --j;
+            }
+        }
+        return smlDiff;
     }
 
 
@@ -527,9 +730,26 @@ public class Test {
         rang2.add("8:30");
         rang2.add("18:00");
         int duration = 60;
-        test.avail(cal1, cal2, rang1, rang2, duration).forEach(strings -> System.out.print(Arrays.toString(strings)));
+//        test.avail(cal1, cal2, rang1, rang2, duration).forEach(strings -> System.out.print(Arrays.toString(strings)));
 
         System.out.println(Arrays.toString(Test.twoNumberSum(new int[] {3, 5, -4, 8, 11, 1, -1, 6}, 10)));
         Test.threeNumberSum(new int[] {12, 3, 1, 2, -6, 5, -8, 6}, 0).forEach(str -> Arrays.toString(str));
+        //1, 1, 1, 1, 1], "sequence": [1, 1, 1]}
+        List<Integer> l = new ArrayList();
+        l.add(1);
+        l.add(1);
+        l.add(1);
+        l.add(1);
+        List<Integer> l2 = new ArrayList();
+        l2.add(1);
+        l2.add(1);
+        l2.add(1);
+        System.out.println(Test.isValidSubsequence(l, l2));
+        System.out.println(Arrays.toString(Test.smallestDifference(new int[] {10, 1000, 9124, 2142, 59, 24, 596, 591, 124, -123}, new int[] {-1441, -124, -25, 1014, 1500, 660, 410, 245, 530})));
+        int[] ar1 = new int[]{1, 2, 3, 4};
+        int[] ar2 = new int[]{12, 13, 14, 5};
+        int[] ar3 = new int[]{11, 16, 15, 6};
+        int[] ar4 = new int[]{10, 9, 8, 7};
+        System.out.println(Test.spiralTraverse(new int[][]{ar1, ar2, ar3, ar4}));
     }
 }
